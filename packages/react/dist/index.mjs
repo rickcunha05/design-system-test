07ac02fdb3dfcd74e6cca52885e6badf984219a2
rickcunha05 <henrique.cunha5@outlook.com>
2023-10-05 18:03:27 -0300

    Criação e implementação do storybook 🎉

--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -1,3 +1,23 @@
+var __defProp = Object.defineProperty;
+var __defProps = Object.defineProperties;
+var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
+var __getOwnPropSymbols = Object.getOwnPropertySymbols;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __propIsEnum = Object.prototype.propertyIsEnumerable;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __spreadValues = (a, b) => {
+  for (var prop in b || (b = {}))
+    if (__hasOwnProp.call(b, prop))
+      __defNormalProp(a, prop, b[prop]);
+  if (__getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(b)) {
+      if (__propIsEnum.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    }
+  return a;
+};
+var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
+
 // ../tokens/dist/index.mjs
 var colors = {
   white: "#FFF",
@@ -15,12 +35,116 @@ var colors = {
   purple500: "#6200ee",
   purple600: "#5900d9"
 };
+var space = {
+  1: "0.25rem",
+  2: "0.5rem",
+  3: "0.75rem",
+  4: "1rem",
+  5: "1.25rem",
+  6: "1.5rem",
+  7: "1.75rem",
+  8: "2rem",
+  10: "2.5rem",
+  12: "3rem",
+  16: "4rem",
+  20: "5rem",
+  40: "10rem",
+  64: "16rem",
+  80: "20rem"
+};
+var lineHeights = {
+  shorter: "125%",
+  short: "140%",
+  base: "160%",
+  tall: "180%"
+};
+var fontWeights = {
+  regular: "400",
+  medium: "500",
+  bold: "700"
+};
+var radii = {
+  px: "1px",
+  xs: "4px",
+  sm: "6px",
+  md: "8px",
+  lg: "16px",
+  full: "99999px"
+};
+var fontSizes = {
+  xxs: "0.625rem",
+  xs: "0.75rem",
+  sm: "0.875rem",
+  md: "1rem",
+  lg: "1.125rem",
+  xl: "1.25rem",
+  "2xl": "1.5rem",
+  "4xl": "2rem",
+  "5xl": "2.25rem",
+  "6xl": "3rem",
+  "7xl": "4rem",
+  "8xl": "4.5rem",
+  "9xl": "6rem"
+};
+var fonts = {
+  default: "Roboto, sans-serif",
+  code: "monospace"
+};
+
+// src/styles/index.ts
+import { createStitches, defaultThemeMap } from "@stitches/react";
+var {
+  styled,
+  css,
+  globalCss,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+  keyframes
+} = createStitches({
+  themeMap: __spreadProps(__spreadValues({}, defaultThemeMap), {
+    //para que o Stitches não subscreva algumas propriedades informamos que queremos apenas adicionar novas e manter também as default
+    height: "space",
+    width: "space"
+  }),
+  theme: {
+    // passando os nossos tokens para o Stitches
+    // uso dos tokens, ai declarar as variáveis do stitches os tokens definidos aparecem com um $ antes deles
+    colors,
+    lineHeights,
+    space,
+    radii,
+    fontSizes,
+    fontWeights,
+    fonts
+  }
+});
 
 // src/index.tsx
-import { jsx } from "react/jsx-runtime";
-function App() {
-  return /* @__PURE__ */ jsx("h1", { style: { color: colors.purple600 }, children: " Hello World " });
-}
+var Button = styled("button", {
+  fontFamily: "$default",
+  backgroundColor: "$purple500",
+  borderRadius: "$md",
+  border: 0,
+  fontWeight: "bold",
+  color: "$white",
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: "$2 $4"
+      },
+      big: {
+        fontSize: 16,
+        padding: "$3 $6"
+      }
+    }
+  },
+  defaultVariants: {
+    size: "small"
+  }
+});
 export {
-  App
+  Button
 };
